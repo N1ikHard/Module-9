257b2d375697e93042057e113fcf51ba767a05c5
n1ik@yandex.ru <n1ik@yandex.ru>
2021-01-26 15:43:14 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TaskOfModule9
+{
+    class MyExeption : Exception
+    {
+        public override string Message
+        {
+            get { return "Message of MyExeption"; }
+        }
+    }
+
+
+    class program
+    {
+        static void Main()
+        {
+            MyExeption obj0=new MyExeption();
+            NullReferenceException obj1=new NullReferenceException();
+            StackOverflowException obj2=new StackOverflowException();
+            DivideByZeroException obj3 = new DivideByZeroException();
+            FormatException obj4 = new FormatException();
+
+            Exception[] array = new Exception[] { obj0,obj1,obj2,obj3,obj4 };
+
+            byte x = 0;
+
+            while (x < 5)
+            {
+                try
+                {
+                    throw array[x];
+                }
+                catch (Exception m)
+                {
+                    Console.WriteLine(m.Message);
+                    x++;
+                }
+            }
+        }
+    }
+}
